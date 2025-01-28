6027a8fecc934d8bdf990769d56979ffb42c156f
EzClick <bitxporg1@gmail.com>
2025-01-28 11:44:55 +0500

    transformation page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -5,35 +5,31 @@ import { getUserById } from '@/lib/actions/user.actions';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = async ({ params: {type}}:
-  SearchParamProps) => {
-    const { userId } = await auth();
-    const tramsformation = transformationTypes[type];
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = await auth();
+  const transformation = transformationTypes[type];
 
-    if(!userId) {
-      redirect('/sign-in');
-      return;
-    }
+  if(!userId) redirect('/sign-in')
 
-    const user = await getUserById(userId);
+  const user = await getUserById(userId);
 
   return (
     <>
       <Header 
-    title={tramsformation.title}
-    subtitle={tramsformation.subTitle} 
-    />
-
-    <TransformationForm 
-      action="Add"
-      userId={user._id}
-      type={transformationTypes[type].type as 
-      TransformationTypeKey}
-      creditBalance={user.creditBalance}
-    />
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+    
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
-
-  )  
+  )
 }
 
 export default AddTransformationTypePage
